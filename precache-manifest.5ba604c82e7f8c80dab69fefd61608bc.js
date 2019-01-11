cfdb0e614727a66fe05ebac365899160a4d5bdf4
Felipe Cotto <felipeoliveira.contato@gmail.com>
2019-01-10 22:44:57 -0200

    Deploy

---
+++ precache-manifest.5ba604c82e7f8c80dab69fefd61608bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4069374b93f9cf68014e",
+    "url": "/static/js/main.4069374b.chunk.js"
+  },
+  {
+    "revision": "979411dac2f07fc5cd17",
+    "url": "/static/js/1.979411da.chunk.js"
+  },
+  {
+    "revision": "4069374b93f9cf68014e",
+    "url": "/static/css/main.27ae88f8.chunk.css"
+  },
+  {
+    "revision": "0c8d6be8649f8fc31f8843a310654c9e",
+    "url": "/index.html"
+  }
+];
